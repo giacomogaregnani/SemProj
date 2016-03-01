8b9938b54990d03f96213d9c580f9dbffe84a05e
GiacomoGaregnani <giacomo.garegnani@epfl.ch>
2016-03-01 11:00:12 +0100

    Added a MAIN function that computes error and plots them for both methods. Changed the tolerance for which the point is considered to be far enough in order to have it as a function of the transport field

--- FIRST/verify_convergence_naive_memory.m
+++ FIRST/verify_convergence_naive_memory.m
@@ -15,8 +15,8 @@ x_bar = [-0.9;-0.9];
 
 % define the integration
 T = 30;
-h = 15./2.^[0:6];
-M = 1000;
+h = 15./2.^[0:8];
+M = 10000;
 n_iter = length(h);
 exp_out = zeros(n_iter,1);
 exp_tau = exp_out;
