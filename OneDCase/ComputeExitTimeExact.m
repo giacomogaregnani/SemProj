075381661d817e4473b88ce2756d940e076ceadb
GiacomoGaregnani <giacomo.garegnani@epfl.ch>
2016-03-07 09:10:06 +0100

    Modified the computation of the exit time exactly considering that f = dV/dx

--- OneDCase/ComputeExitTimeExact.m
+++ OneDCase/ComputeExitTimeExact.m
@@ -1,12 +1,14 @@
-function ComputeExitTimeExact(X0,f,g,Bounds,BoundCond)
+function ComputeExitTimeExact(X0,V,dV,g,Bounds,BoundCond)
 
 l = Bounds(1);
 r = Bounds(2);
 
-psi = @(x) integral(f,l,x);
+psi = @(x) 2*((V(x) - V(l))./(g(x).^2)) .* (x-l);
 
 FunFirstTermInside = @(z) exp(psi(z)) ./ (g(z).^2);
-FirstTermInside = @(y) exp(-psi(y)) .* (y/NInt) .* cumtrapz(FunFirstTermInside(linspace(0,y,NInt));
-FirstTerm = -2 * integral(FirstTermInside,l,X0);
+FirstTermInside = @(y,z) exp(-psi(y)) .* FunFirstTermInside(z);
+LimSupInt = @(y) y;
+FirstTerm = -2 * integral2(FirstTermInside,l,X0,l,LimSupInt);
+
 
 end
--- OneDCase/Main.m
+++ OneDCase/Main.m
@@ -2,8 +2,9 @@
 
 % Define the problem 
 Time = [0,100];
-f = @(x) -0.1 * (x + 0.5);
-g = @(x) 0.05;
+V = @(x) -0.1 * (x.^2/2 + x);
+dV = @(x) -0.1 * (x + 1);
+g = @(x) 0.04;
 X0 = 0.5;
 Bounds = [-1,1];
 N = 100;
@@ -13,6 +14,6 @@ M = 10;
 W = BrownianMotion(Time,N,M);
 
 % Compute the exit time expectation
-tau = ComputeExitTimeNaive(X0,f,g,Bounds,0,W,Time);
+tau = ComputeExitTimeNaive(X0,dV,g,Bounds,0,W,Time);
 
-TrajectoriesForPlots(X0,f,g,Bounds,0,W,Time);
+TrajectoriesForPlots(X0,dV,g,Bounds,0,W,Time);
