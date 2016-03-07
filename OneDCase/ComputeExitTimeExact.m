03974fe2a89aabaac59690d66ea7ea050154dda0
GiacomoGaregnani <giacomo.garegnani@epfl.ch>
2016-03-07 17:49:45 +0100

    Corrected numerical and exact computations. The order 0.5 is found for the naive implementation

--- OneDCase/ComputeExitTimeExact.m
+++ OneDCase/ComputeExitTimeExact.m
@@ -3,9 +3,9 @@ function tau = ComputeExitTimeExact(X0,V,g,Bounds,BoundCond)
 l = Bounds(1);
 r = Bounds(2);
 
-psi = @(x) 2 * ( V(l) - V(x) ) ./ g(x); % Formula is valid only if g is constant and f = -V'.
+psi = @(x) 2 * ( V(l) - V(x) ) ./ (g(x).^2); % Formula is valid only if g is constant and f = -V'.
 
-FirstTermArg = @(x,y) exp(-psi(x)) .* exp(psi(y)) ./ g(y);
+FirstTermArg = @(x,y) exp(-psi(x)) .* exp(psi(y)) ./ (g(y).^2);
 LimSupInt = @(x) x;
 FirstTerm = integral2(FirstTermArg,l,X0,l,LimSupInt);
 
@@ -15,7 +15,7 @@ SecondTerm = integral(SecondTermArg,l,X0);
 if BoundCond(2) == 0
     c1 = 2 * integral2(FirstTermArg,l,r,l,LimSupInt) / integral(SecondTermArg,l,r);
 else
-    c1Arg = @(z) exp(psi(z)) ./ g(z);
+    c1Arg = @(z) exp(psi(z)) ./ (g(z).^2);
     c1 = 2 * integral(c1Arg,l,r);
 end
 
--- OneDCase/Main.m
+++ OneDCase/Main.m
@@ -7,12 +7,12 @@ Time = [0,10];
 V = @(x) 0.01 * (8 * x.^4 - 8 * x.^2 + x + 2);
 dV = @(x)  0.01 * (32 * x.^3 - 16 * x + 1);
 f = @(x) -1 * dV(x);
-g = @(x) 0.5;
+g = @(x) 1;
 X0 = 0;
 Bounds = [-1,1];
 BoundCond = [0,0];
-N = 2.^[5:10];
-M = 1000;
+N = 2.^[5:12];
+M = 1500;
 
 % Compute the BM
 W = BrownianMotion(Time,N(end),M);
@@ -26,17 +26,23 @@ for i = 1:length(N)
     length(N) - i
 end
 
-figure
-TrajectoriesForPlots(X0,f,g,Bounds,0,W,Time);
+% Plot the trajectories for the finest timestep (Comment for memory and CPUT save)
+% figure
+% TrajectoriesForPlots(X0,f,g,Bounds,0,W,Time);
 
+% Compute the exact expectation of tau and the error
 tauEx = ComputeExitTimeExact(X0,V,g,Bounds,BoundCond);
 errNaive = abs(tauNaive - tauEx);
 
+% Plot the error for orders analysis
+h = (Time(2)-Time(1))./N;
 figure
-loglog((Time(2)-Time(1))./N,errNaive,'ko-')
+loglog(h,errNaive,'ro-')
 hold on
-loglog((Time(2)-Time(1))./N,1./sqrt(N),'r--')
+loglog(h,sqrt(h)*(errNaive(3)/sqrt(h(3))),'k--')
+grid on
 
+% Compute the orders
 OrdersNaive = log2(errNaive(1:end-1)./errNaive(2:end));
 
 figure
