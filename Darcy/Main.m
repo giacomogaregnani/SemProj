15027e98ffc4f94b0ae0a7bf5b4a6e4f7ebe1233
GiacomoGaregnani <giacomo.garegnani@epfl.ch>
2016-03-22 16:46:53 +0100

    Code for Darcy problem is working in any cases but it is too slow for double MC. Added routine that plots trajectories for correctness check, everything seems correct. Just started the report part about Darcy (problem statement).

--- Darcy/Main.m
+++ Darcy/Main.m
@@ -6,18 +6,18 @@ clc
 % Solution of the transport diffusion SDE with velocity field v = -Ax;
 
 % Define the problem 
-Time = [0,1];
-sigma = 1;
+Time = [0,30];
+sigma = 0.3;
 
 V = @(x,y) zeros(2,1) * x * y;
 dV = @(x,y) zeros(2,1) * x * y;
 f = @(x,y) -dV(x,y);
 g = @(x,y) sigma * eye(2);
-X0 = [-0.8;0];
+X0 = [-0.8;-0.8];
 Bounds = [-1,1;-1,1];
 BoundCond = 1; % 0 for killing everywhere. 1 for two killing and two reflecting BCs.
-N = 2.^[0:3];
-M = 1000;
+N = 2.^[0:5];
+M = 1e4;
 
 NRef = N(end)*32;
 
@@ -36,9 +36,11 @@ tBernoulli = tauNaive;
 [Pressure,A] = SolveDarcy(1,1,'True');
 
 for i = 1:length(N)
-    % Compute the exit time expectation
+%     Compute the exit time expectation
     [tauNaive(i),phiNaive(i),tNaive(i)] = ComputeExitTimeNaiveDarcy(X0,f,g,Bounds,BoundCond,W(:,1:N(end)/N(i):end),Time,Pressure,A);
     [tauBernoulli(i),phiBernoulli(i),tBernoulli(i)] = ComputeExitTimeBernoulliDarcy(X0,f,g,Bounds,BoundCond,W(:,1:N(end)/N(i):end),Time,Pressure,A);
+%     ComputeExitTimeNaiveDarcyPlot(X0,f,g,Bounds,BoundCond,W(:,1:N(end)/N(i):end),Time,Pressure,A);
+    
     length(N) - i
 end
 
