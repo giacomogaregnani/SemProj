036f7f639c44b1ffbde8bf68b91e1807ee38f66f
GiacomoGaregnani <giacomo.garegnani@epfl.ch>
2016-03-31 18:07:20 +0200

    Changed with implicit Euler to increase stability wrt sigma, f, h, dx

--- FiniteDiff/Test.m
+++ FiniteDiff/Test.m
@@ -4,39 +4,57 @@ close all
 
 % Set up of time span and space interval
 dx = 0.1;
-h = 0.001 * dx^2;
+h = 0.01;
 x = -1:dx:1;
-T = 0.5;
+T = 5;
 t = 0:h:T;
 
 % Boundary Conditions
-N = length(x);
+N = length(x) - 2;
 M = length(t);
-u = zeros(N,M);
+u = zeros(N + 2,M);
 BoundCond = 1;
-r = h / (dx^2);
 
 % f and sigma
-f = 0.1 * (32 * x.^3 - 16 * x + 1)';
+f =  @(x) -0.1 * (32 * x.^3 - 16 * x + 1)';
 sigma = 1;
 
-
 if BoundCond == 0
-    u = [[1;zeros(N-2,1);1],[ones(1,M-1);zeros(N-2,M-1);ones(1,M-1)]];
+    LDiag = -(-f(x(3:end-1)) * h / (2*dx) + sigma^2 * h / (2 * dx^2));
+    UDiag = -(f(x(2:end-2)) * h / (2*dx) + sigma^2 * h / (2 * dx^2));
+    Diag = ones(N,1) + h / (dx^2) * sigma^2;
+    A = diag(Diag) + diag(LDiag,-1) + diag(UDiag,1);
+    A = sparse(A);
+    BoundLeft = -f(x(2)) * h / (2 * dx) + sigma^2 * h / (2 * dx^2);
+    BoundRight = f(x(end-1)) * h / (2 * dx) + sigma^2 * h / (2 * dx^2);
+    
+    u = [ones(1,M);zeros(N,M);ones(1,M)];
     
     for j = 2 : M
-        u(2:end-1,j) = (1 - r*sigma^2) * u(2:end-1,j-1) + (f(2:end-1)*dx*r + 0.5*sigma^2*r) .* u(1:end-2,j-1) + (-f(2:end-1)*dx*r + 0.5*sigma^2*r) .* u(3:end,j-1);
+        u(2:end-1,j) = A \ [u(2,j-1) + BoundLeft; u(3:end-2,j-1); u(end-1,j-1) + BoundRight];
     end
     
-    figure
-    plot(x,u(:,end),'o-')
 elseif BoundCond == 1
-    u = [[1;zeros(N-1,1)],[ones(1,M-1);zeros(N-1,M-1)]];
+    LDiag = -(-f(x(3:end)) * h / (2*dx) + sigma^2 * h / (2 * dx^2));
+    UDiag = -(f(x(2:end-1)) * h / (2*dx) + sigma^2 * h / (2 * dx^2));
+    Diag = ones(N+1,1) + h / (dx^2) * sigma^2;
+    A = diag(Diag) + diag(LDiag,-1) + diag(UDiag,1);
+    A(end,end-1) = 1;
+    A(end,end) = -1;
+    A = sparse(A);
+    BoundLeft = -f(x(2)) * h / (2 * dx) + sigma^2 * h / (2 * dx^2);
+    
+    u = [ones(1,M);zeros(N+1,M)];
+
     for j = 2 : M
-        u(2:end-1,j) = (1 - r*sigma^2) * u(2:end-1,j-1) + (f(2:end-1)*dx*r + 0.5*sigma^2*r) .* u(1:end-2,j-1) + (-f(2:end-1)*dx*r + 0.5*sigma^2*r) .* u(3:end,j-1);
-        u(end,j) = u(end-1,j);
+        u(2:end,j) = A \ [u(2,j-1) + BoundLeft; u(3:end-1,j-1); 0];
     end
     
-    figure
-    plot(x,u(:,end),'o-')
 end
+
+figure
+plot(x,u(:,end),'o-')
+
+
+
+
